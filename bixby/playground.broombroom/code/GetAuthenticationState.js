4d1325e10336c042d0bb1cef762f6610f4541641
이수영 <swimming_7291@daum.net>
2020-05-29 07:24:08 +0000

    Revert "Merge branch 'bixby/README' into 'develop'"
    This reverts merge request !3

--- bixby/playground.broombroom/code/GetAuthenticationState.js
+++
@@ -1,8 +0,0 @@
-module.exports.function = function getAuthenticationState (authenticationCode) {
-  var authenticationState = {
-    authenticationCode: authenticationCode,
-    isAuthenticated: false,
-  }
-  
-  return authenticationState
-}
