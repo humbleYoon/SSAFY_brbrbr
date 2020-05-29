3fee3d5fe5a6900b8193dd53f2c1ded02efc84f7
YoonJaeKim <rladbswo1124@naver.com>
2020-05-29 16:30:26 +0900

    [feat] Add transaction_v2 Modeling GuidePage

--- bixby/playground.broombroom/code/UpdateOrder.js
+++ bixby/playground.broombroom/code/UpdateOrder.js
@@ -1,16 +1,27 @@
 var console = require('console');
 
-module.exports.function = function updateOrder (order, changedAuthenticationState, addGuideType, searchTerm, isArrived) {
+module.exports.function = function updateOrder (order, changedAuthenticationState, changedItem, searchTerm, isArrived) {
   var nextOrder = order;
 
-  if (changedAuthenticationState != undefined) {
+  if (order.step == "인증 페이지") {
     // 백엔드와 authenticationCode로 통신 후 결과 처리
     const retState = {
       authenticationCode: changedAuthenticationState.authenticationCode,
       isAuthenticated: changedAuthenticationState.authenticationCode == 1124,
     };
-      
+
+    if (changedAuthenticationState.authenticationCode == 1124) {
+      nextOrder.step = "가이드 타입 선택 페이지"
+    }
+
     nextOrder.authenticationState = retState;
+    console.log(nextOrder);
+  }
+  else if (order.step == "가이드 타입 선택 페이지") {
+    
+    nextOrder.step = "안내 페이지"
+
+    nextOrder.item = changedItem;
     console.log(nextOrder);
   }
   
