bf5d72d4ee1a2962bd01e6c81f1b48e4f87ed3ec
YoonJaeKim <rladbswo1124@naver.com>
2020-05-31 17:49:53 +0900

    [feat] Add bixby Modeling

--- bixby/playground.broombroom/code/CreateOrder.js
+++ bixby/playground.broombroom/code/CreateOrder.js
@@ -1,19 +1,13 @@
 var console = require('console');
-const placeAndEventList = require('lib/placeAndEventList');
 
-module.exports.function = function createOrder (authenticationState, item, step) {
+module.exports.function = function createOrder () {
   var order = {
     authenticationState: {
       authenticationCode: 1124,
       isAuthenticated: false,
     },
-    item: {
-      guideType: "안내",
-      placeAndEvent: placeAndEventList,
-      robotState: "정지",
-    },
     orderNumber: 1,
-    step: "인증 페이지",
+    step: "인증",
   }
 
   console.log(order);
--- bixby/playground.broombroom/code/UpdateOrder.js
+++ bixby/playground.broombroom/code/UpdateOrder.js
@@ -1,9 +1,9 @@
 var console = require('console');
 
-module.exports.function = function updateOrder (order, changedAuthenticationState, changedItem, searchTerm, isArrived) {
+module.exports.function = function updateOrder (order, changedAuthenticationState, changedItem, selectedPlaceName, selectedEventName, currentRobotState) {
   var nextOrder = order;
 
-  if (order.step == "인증 페이지") {
+  if (order.step == "인증") {
     // 백엔드와 authenticationCode로 통신 후 결과 처리
     const retState = {
       authenticationCode: changedAuthenticationState.authenticationCode,
@@ -11,18 +11,44 @@ module.exports.function = function updateOrder (order, changedAuthenticationStat
     };
 
     if (changedAuthenticationState.authenticationCode == 1124) {
-      nextOrder.step = "가이드 타입 선택 페이지"
+      nextOrder.step = "가이드 타입 선택"
     }
 
     nextOrder.authenticationState = retState;
     console.log(nextOrder);
   }
-  else if (order.step == "가이드 타입 선택 페이지") {
+  else if (order.step == "가이드 타입 선택") {
     
-    nextOrder.step = "안내 페이지"
+    nextOrder.step = "안내"
 
     nextOrder.item = changedItem;
     console.log(nextOrder);
+  }
+  else if (order.step == "안내") {
+    const retDestination;
+    if (selectedPlaceName) {
+      retDestination = {
+        destinationName: selectedPlaceName,
+        destinationDescription: "장소에 대한 설명이 들어갑니다",
+      };
+    }
+    else {
+      retDestination = {
+        destinationName: selectedEventName,
+        destinationDescription: "행사에 대한 설명이 들어갑니다.",
+      };
+    }
+    nextOrder.destination = retDestination
+    nextOrder.step = "이동중"
+    nextOrder.pressedCount = 0
+  }
+  else if (order.step == "이동중") {
+    if (currentRobotState == "도착") {
+      nextOrder.step = "도착"
+    }
+    else{
+      nextOrder.pressedCount += 1
+    }
   }
   
   return nextOrder;
