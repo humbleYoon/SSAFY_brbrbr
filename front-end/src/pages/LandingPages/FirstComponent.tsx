72b9e0f6d87aa6f8d4afb444eb51c8d9931d0885
An yulim <db9292@naver.com>
2020-06-08 20:31:36 +0900

    [Feat] landing layout

---
+++ front-end/src/pages/LandingPages/FirstComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="component first-component">
+      <h2>부릉부릉</h2>
+    </div>
+  );
+};
---
+++ front-end/src/pages/LandingPages/ThirdComponent.tsx
@@ -0,0 +1,10 @@
+  
+import React from "react";
+
+export default () => {
+  return (
+    <div className="component third-component">
+      <h2>Third Component</h2>
+    </div>
+  );
+};
