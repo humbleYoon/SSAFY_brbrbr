c93ff478be15d4df588344990938302b16f00371
anyl92 <yulim9592@gmail.com>
2020-06-11 01:03:03 +0900

    [Feat] landing first page

--- front-end/src/pages/LandingPages/FirstComponent.tsx
+++ front-end/src/pages/LandingPages/FirstComponent.tsx
@@ -1,19 +1,43 @@
 /** @jsx jsx  */
 import React from "react";
 import { css, jsx } from '@emotion/core'
+import robot from '../../assets/tmpImage.png'
 
+const boxPadding = css`
+  height: 20vw;
+  visibility: hidden;
+  position: relative;
+`
 
 const fontPadding = css`
   padding-top: 20px;
+  width: 100%;
+  font-size: 5.5rem;
+`
+
+const fontBox = css`
+  width: 67vw;
+  display: inline-block;
+`
+
+const imageBox = css`
+  width: 33vw;
+  display: inline-block;
+  position: absolute;
+  bottom: 0px;
 `
 
 export default () => {
   return (
     <div>
-      <div>
-        <h2>자율주행</h2>
+      <div css={boxPadding}>영역을차지하렴</div>
+      <div css={fontBox}>
+        <h2 css={fontPadding}>자율주행</h2>
         <h2 css={fontPadding}>안내로봇</h2>
         <h2 css={fontPadding}>「로미」</h2>
+      </div>
+      <div css={imageBox}>
+        <img src={robot} alt='로봇사진' width="300px" height="500px"></img>
       </div>
     </div>
   );
--- front-end/src/pages/LandingPages/ThirdComponent.tsx
+++ front-end/src/pages/LandingPages/ThirdComponent.tsx
@@ -1,10 +1,18 @@
-  
+/** @jsx jsx  */
 import React from "react";
+import { css, jsx } from '@emotion/core'
+
+const boxPadding = css`
+  height: 10vw;
+  visibility: hidden;
+  position: relative;
+`
 
 export default () => {
   const text = '영상으로 구경해보시죠 \>_~'
   return (
     <div>
+      <div css={boxPadding}>영역을차지하렴</div>
       <h2>{text}</h2>
       <div className='videoTag'>동영상</div>
     </div>
