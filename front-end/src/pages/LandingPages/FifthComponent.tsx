c383352e80ae5a49b1cd84c7a248050167dfe534
An yulim <db9292@naver.com>
2020-06-17 19:00:17 +0900

    [Feat] picture hover effect

--- front-end/src/pages/LandingPages/FifthComponent.tsx
+++ front-end/src/pages/LandingPages/FifthComponent.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { css, jsx } from '@emotion/core'
 
+import './profile.css'
+
 const wrapper = css`
   display: grid;
   height: 100%;
@@ -73,12 +75,50 @@ export default () => {
         안유림 AN YU LIM <br />
         장현진 JANG HYEON JIN <br />
       </div>
-      <div css={KYJ}><img src='/image/KIM YOON JAE.jpg' css={image} width='100%' height='100%' /></div>
-      <div css={KHC}><img src='/image/KIM HYEON CHEOL.jpg' css={image} width='100%' height='100%'  /></div>
-      <div css={BSH}><img src='/image/BAE SEONG HO.jpg' css={image} width='100%' height='100%' /></div>
-      <div css={AYL}><img src='/image/AN YU LIM.jpg' css={image} width='100%' height='100%' /></div>
-      <div css={LSY}><img src='/image/LEE SOO YOUNG.jpg' css={image} width='100%' height='100%' /></div>
-      <div css={JHJ}><img src='/image/JANG HYEON JIN.jpg' css={image} width='100%' height='100%' /></div>
+      <div className="snip1273" css={KYJ}><img src='/image/KIM YOON JAE.jpg' css={image} width='100%' height='100%' />
+        <figcaption>
+          <h3 style={{color:'white'}}>KIM YOON JAE</h3>
+          <div>Bixby</div>
+          <div>빅스비는 로컬호스트를 몰라요..</div>
+        </figcaption>
+      </div>
+      <div className="snip1273" css={KHC}><img src='/image/KIM HYEON CHEOL.jpg' css={image} width='100%' height='100%' />
+        <figcaption>
+          <h3 style={{color:'white'}}>KIM HYEON CHEOL</h3>
+          <div>Robot Drive Part</div>
+          <div>어이쿠</div>
+        </figcaption>
+      </div>
+      <div className="snip1273" css={BSH}><img src='/image/BAE SEONG HO.jpg' css={image} width='100%' height='100%' />
+        <figcaption>
+          <h3 style={{color:'white'}}>BAE SEONG HO</h3>
+          <div>Web FullStack</div>
+          <div>다음 프로젝트는 앱으로</div>
+        </figcaption> 
+      </div>
+      <div className="snip1273" css={AYL}><img src='/image/AN YU LIM.jpg' css={image} width='100%' height='100%' />
+        <figcaption>
+          <h3 style={{color:'white'}}>AN YU LIM</h3>
+          <div>Robot Communication</div>
+          <div>Front-end</div>
+          <div>로미로미</div>
+        </figcaption> 
+      </div>
+      <div className="snip1273" css={LSY}><img src='/image/LEE SOO YOUNG.jpg' css={image} width='100%' height='100%' />
+        <figcaption>
+          <h3 style={{color:'white'}}>LEE SOO YOUNG</h3>
+          <div>Project Manager</div>
+          <div>Robot Odometry</div>
+          <div>Atchacha.... 로봇은 취미로</div>
+        </figcaption> 
+      </div>
+      <div className="snip1273" css={JHJ}><img src='/image/JANG HYEON JIN.jpg' css={image} width='100%' height='100%' />
+        <figcaption>
+          <h3 style={{color:'white'}}>JANG HYEON JIN</h3>
+          <div>Bixby</div>
+          <div>하이 빅스비~!</div>
+        </figcaption> 
+      </div>
     </div>
   );
 };
